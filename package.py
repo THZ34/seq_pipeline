409cdfe5b6f59eef3fae03d605c9e0f0a79e182d
THZ34 <tanghongzhen34@gmail.com>
2020-11-03 16:48:58 +0800

    build: add cripsr-screening preprocess

---
+++ package.py
@@ -0,0 +1,10 @@
+
+package = {
+    'name': 'pipeline',
+    'version': '0.0.1',
+    'author': 'Tang Hongzhen',
+    'email': 'tanghongzhen34@gmail.com',
+    'scripts': {
+        'default': 'echo 请输入明确的命令名称'
+    }
+}
---
+++ pipeline/setup.py
@@ -0,0 +1,11 @@
+# coding='utf-8'
+from setuptools import setup
+
+setup(
+    name='seq_pipeline',
+    version='0.0.1',
+    description='Pipeline for sequencing data',
+    author='Tang Hongzhen',
+    author_email='tanghongzhen34@gmail.com',
+    packages=['demo'],
+)
